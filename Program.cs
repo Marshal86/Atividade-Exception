580f55ea0a416b3c7943891c61c458bf0b439548
Eduardo Marçal Clemente <80399639+Marshal86@users.noreply.github.com>
2022-09-09 17:37:48 -0300

    O que o programa faz?
    Fazer um programa para ler os dados de uma conta bancária e depois realizar um
    saque nesta conta bancária, mostrando o novo saldo. Um saque não pode ocorrer
    ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de
    saque da conta.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Exceções.Entities;
+using System;
+using Exceções.Entities.Exceptions;
+namespace Exceções
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                Console.WriteLine("Enter account data :");
+                Console.Write("Number :");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Name :");
+                string name = Console.ReadLine();
+                Console.Write("Initial Balance :");
+                double initialbalance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw Limit :");
+                double witdrawlimit = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Conta conta = new Conta(number, name, initialbalance, witdrawlimit);
+
+                Console.Write("Enter amount for withdraw :");
+                double amount = double.Parse(Console.ReadLine());
+                conta.Withdraw(amount);
+
+                Console.WriteLine(conta);
+
+            }
+
+            catch(DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+ 
+
+        }
+    }
+}
